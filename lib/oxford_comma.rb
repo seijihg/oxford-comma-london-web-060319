635d8e8e46053dc6182733a260a677e61e8767bf
Le H Ngo <seiji@o2.pl>
2019-05-21 11:15:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length <= 2
+    joined = array.join(" and ")
+  elsif array.length == 3
+    joined = array.join(", ")
+    joined.insert(14, "and ")
+  else
+    joined = array.join(", ")
+    joined.reverse.sub(",", "dna ,").reverse
+  end
 end
