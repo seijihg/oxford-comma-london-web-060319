fa54e97ccefccf4ea85fdd49e659f571663659de
Le H Ngo <seiji@o2.pl>
2019-05-21 11:08:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,10 @@
 def oxford_comma(array)
   if array.length <= 2
     joined = array.join(" and ")
-  else
+  elsif array.length == 3
     joined = array.join(", ")
     joined.insert(14, "and ")
+  else
+    joined = array.join(", ")
   end
 end
