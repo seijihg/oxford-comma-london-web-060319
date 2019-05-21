ae87a2ee0d5b9b944f3e93646870f8d7f2b0d7c6
Le H Ngo <seiji@o2.pl>
2019-05-21 10:32:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length <= 2
     joined = array.join(" and ")
   else
-    joined = array.join(" ")
+    joined = array.join(", ")
     joined.sub("and", ",")
   end
 end
